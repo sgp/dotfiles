e5c6c06e567a41dc1a9e17a3bf9cadaaf27a7eb4
Scott Parkerson <scott@parkerson.net>
2012-03-08 17:27:32 -0500

    Additions for bash, .ssh/rc to setup.sh

--- setup.sh
+++ setup.sh
@@ -8,5 +8,9 @@ fi
 [ ! -L $HOME/.vimrc ] && ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
 [ ! -L $HOME/.vim ] && ln -s $DOTFILES/vim $HOME/.vim
 [ ! -L $HOME/.screenrc ] && ln -s $DOTFILES/screen/screenrc $HOME/.screenrc
+[ ! -L $HOME/.bashrc ] && ln -s $DOTFILES/bash/bashrc $HOME/.bashrc
+[ ! -L $HOME/.bash_profile ] && ln -s $DOTFILES/bash/bash_profile $HOME/.bash_profile
+[ ! -L $HOME/.bash_logout ] && ln -s $DOTFILES/bash/bash_logout $HOME/.bash_logout
+[ ! -L $HOME/.ssh/rc ] && ln -s $DOTFILES/ssh/rc $HOME/.ssh/rc
 
 exit 0
